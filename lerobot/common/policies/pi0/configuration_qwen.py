43ba0bb7e7633235f05d2d81920d250b5f68a5ef
lzlglory <lizuolei24@ime.ac.cn>
2025-06-11 15:02:31 +0000

    fix bug

--- lerobot/common/policies/pi0/configuration_qwen.py
+++ lerobot/common/policies/pi0/configuration_qwen.py
@@ -12,9 +12,9 @@ from lerobot.configs.types import FeatureType, NormalizationMode, PolicyFeature
 @dataclass
 class QwenConfig(PreTrainedConfig):
 
-    # qwen_path: str = "/mnt/wangxiaofa/qwen_params/Qwen2.5-VL-7B-Instruct/"
+    qwen_path: str = "/mnt/wangxiaofa/qwen_params/Qwen2.5-VL-7B-Instruct/"
     # qwen_path = "/datassd_1T/qwen25vl/Qwen2.5-VL-7B-Instruct/"
-    qwen_path: str = "/Data/lzl/qwen2.5_vl_7b/Qwen2.5-VL-7B-Instruct"
+    # qwen_path: str = "/Data/lzl/qwen2.5_vl_7b/Qwen2.5-VL-7B-Instruct"
     
     # Input / output structure.
     n_obs_steps: int = 1
