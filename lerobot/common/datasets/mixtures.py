fd6b11d3daa1ffb8d04bbfb8f3504fcb5af2e6ce
lzlglory <lizuolei24@ime.ac.cn>
2025-06-29 07:08:48 +0000

    fractal_v3

--- lerobot/common/datasets/mixtures.py
+++ lerobot/common/datasets/mixtures.py
@@ -257,6 +257,9 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
      "simpler_fractal_sub2_v2":[
          ("simpler_fractal_sub2_v2", 1.0), 
      ],
+     "simpler_fractal_sub2_v3":[
+         ("simpler_fractal_sub2_v3", 1.0), 
+     ],
      "simpler_bridge":[
          ("simpler_bridge", 1.0), 
      ],
