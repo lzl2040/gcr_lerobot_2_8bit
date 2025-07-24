05f68bf7e3f87e961c0800824e23fa1a4e21e993
lzlglory <lizuolei24@ime.ac.cn>
2025-07-24 16:54:58 +0000

    fix bug

--- lerobot/scripts/fsdp_train.py
+++ lerobot/scripts/fsdp_train.py
@@ -546,7 +546,7 @@ if __name__ == "__main__":
     # os.environ["TOKENIZERS_PARALLELISM"] = "false"
     # os.environ["OMPI_ALLOW_RUN_AS_ROOT"] = "1"
     # os.environ["OMPI_ALLOW_RUN_AS_ROOT_CONFIRM"] = "1"
-    # os.environ['WANDB_API_KEY'] = '9e1c3ac77856b8ebb5573c4e1e250c84aabfb904'
+    os.environ['WANDB_API_KEY'] = '9e1c3ac77856b8ebb5573c4e1e250c84aabfb904'
     
     # 启动训练
     train()
