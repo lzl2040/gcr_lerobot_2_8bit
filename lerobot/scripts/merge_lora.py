6d04a4460a6a1c42747cf4ffe9cb40e2b2579733
lzlglory <lizuolei24@ime.ac.cn>
2025-08-02 15:21:13 +0000

    update task_9_sep1

--- lerobot/scripts/merge_lora.py
+++ lerobot/scripts/merge_lora.py
@@ -289,7 +289,7 @@ def train(cfg: TrainPipelineConfig):
     logger.info("Still creating policy...")
     
     
-    weight_root = "/mnt/wangxiaofa/original_qw/0729_exp+01_robotics_df100_full_ft_pizza_task_20_wd_1e-10_w_aug"
+    weight_root = "/mnt/wangxiaofa/original_qw/0729_exp+01_robotics_df100_full_ft_pizza_task_9_sep1_wd_1e-10_w_aug/"
     
     for step in range(1000, 20000, 1000):
         weight_path = os.path.join(weight_root, f"step{step}.pt")
