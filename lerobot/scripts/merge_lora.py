72011c0de0aad33986876c0e331285e62059a55a
lzlglory <lizuolei24@ime.ac.cn>
2025-08-08 00:27:36 +0000

    merge_lora_task5_v2

--- lerobot/scripts/merge_lora.py
+++ lerobot/scripts/merge_lora.py
@@ -296,7 +296,7 @@ def train(cfg: TrainPipelineConfig):
     # )
     
     
-    weight_root = "/mnt/wangxiaofa/original_qw/0806_exp+02_robotics_df100_full_ft_pizza_task_5_v2_wd_1e-10_w_aug"
+    weight_root = "/mnt/wangxiaofa/original_qw/0806_exp+04_robotics_df100_full_ft_pizza_task_5_v2_wd_1e-10_w_aug"
     
     for step in range(3000, 10000, 500):
         weight_path = os.path.join(weight_root, f"step{step}.pt")
